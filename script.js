8863d51e1f0327fdf54690f5af7fb1f908605b79
reda777 <redazara1998@gmail.com>
2022-04-15 14:05:54 +0000

    Finish 2 players script

--- script.js
+++ script.js
@@ -27,39 +27,49 @@ const gameBoard = (() => {
         mLine1 : [],
         mLine2 : []
     };
-    const boardSquares = document.querySelectorAll(".board > div")
+    const clickSquare = function(){
+        if(user1.getTurn()==1){
+            this.textContent=user1.shape;
+            user1.toggleTurn();
+            user2.toggleTurn();
+            fillLinesArray(this,user1.shape);
+            this.removeEventListener("click", clickSquare);
+            checkWinner();
+        }else if(user1.getTurn()==0){
+            this.textContent=user2.shape;
+            user2.toggleTurn();
+            user1.toggleTurn();
+            fillLinesArray(this,user2.shape);
+            this.removeEventListener("click", clickSquare);
+            checkWinner();
+        }
+    }
+    const fillLinesArray = function(that,playerShape){
+        const currentSquareClasses=that.className.split(" ");
+            currentSquareClasses.forEach(item=>{
+                boardLines[item].push(shape[playerShape]);
+        });
+    }
+    const  checkWinner = function(){
+        for(let line in boardLines){
+            if(boardLines[line].reduce((a,b)=>a+b,0)==3){
+                console.log("x won");
+                finishGame();
+            }
+            else if(boardLines[line].reduce((a,b)=>a+b,0)==-3){
+                console.log("o won");
+                finishGame();
+            }
+        }
+    }
+    const boardSquares = document.querySelectorAll(".board > div");
+    const  finishGame = function(){
+        boardSquares.forEach((square) => {
+            square.removeEventListener("click", clickSquare);
+        });
+    }
     boardSquares.forEach((square) => {
         square.addEventListener("click", clickSquare);
     });
-    return{boardLines};
 })();
 
-function clickSquare(){
-    if(user1.getTurn()==1){
-        this.textContent=user1.shape;
-        user1.toggleTurn();
-        user2.toggleTurn();
-        fillBoardLines(this,user1.shape);
-        this.removeEventListener("click", clickSquare);
-    }else if(user1.getTurn()==0){
-        this.textContent=user2.shape;
-        user2.toggleTurn();
-        user1.toggleTurn();
-        fillBoardLines(this,user2.shape);
-        this.removeEventListener("click", clickSquare);
-    }
-    for(let line in gameBoard.boardLines){
-        if(gameBoard.boardLines[line].reduce((a,b)=>a+b,0)==3){
-            console.log("x won");
-        }
-        else if(gameBoard.boardLines[line].reduce((a,b)=>a+b,0)==-3){
-            console.log("o won");
-        }
-    }
-}
-function fillBoardLines(that,playerShape){
-    const currentSquareClasses=that.className.split(" ");
-        currentSquareClasses.forEach(item=>{
-            gameBoard.boardLines[item].push(shape[playerShape]);
-    });
-}
