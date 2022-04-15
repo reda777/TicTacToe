d1fad56cbc3d3a604e5a35bf32b5b229ed2f556e
reda777 <redazara1998@gmail.com>
2022-04-15 12:31:44 +0000

    Finish 2 players script

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
-const player = function(shape){
-    return{shape};
+const shape={
+    "x" : 1,
+    "o" : -1
 }
-const user1=player("x");
-const user2=player("o");
+const player = (shape, turn) => {
+    const getTurn = () => turn;
+    const toggleTurn = () =>{
+        if(turn==1){
+            turn=0;
+        }
+        else{
+            turn=1;
+        }
+    };
+    return{shape, getTurn, toggleTurn};
+};
+let user1=player("x",1);
+let user2=player("o",0);
 const gameBoard = (() => {
     let boardLines = {
         hLine1 : [],
@@ -22,14 +35,31 @@ const gameBoard = (() => {
 })();
 
 function clickSquare(){
-    this.textContent=user1.shape;
-    const currentSquareClasses=this.className.split(" ");
-    currentSquareClasses.forEach(item=>{
-        gameBoard.boardLines[item].push(1);
-    });
+    if(user1.getTurn()==1){
+        this.textContent=user1.shape;
+        user1.toggleTurn();
+        user2.toggleTurn();
+        fillBoardLines(this,user1.shape);
+        this.removeEventListener("click", clickSquare);
+    }else if(user1.getTurn()==0){
+        this.textContent=user2.shape;
+        user2.toggleTurn();
+        user1.toggleTurn();
+        fillBoardLines(this,user2.shape);
+        this.removeEventListener("click", clickSquare);
+    }
     for(let line in gameBoard.boardLines){
         if(gameBoard.boardLines[line].reduce((a,b)=>a+b,0)==3){
-            console.log("stop");
+            console.log("x won");
+        }
+        else if(gameBoard.boardLines[line].reduce((a,b)=>a+b,0)==-3){
+            console.log("o won");
         }
     }
+}
+function fillBoardLines(that,playerShape){
+    const currentSquareClasses=that.className.split(" ");
+        currentSquareClasses.forEach(item=>{
+            gameBoard.boardLines[item].push(shape[playerShape]);
+    });
 }
