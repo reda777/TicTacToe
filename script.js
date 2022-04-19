6216efdb121347a6013a0880b1eefaea95938329
reda777 <redazara1998@gmail.com>
2022-04-19 12:37:09 +0000

    Make button works (kinda)

--- script.js
+++ script.js
@@ -12,6 +12,13 @@ const player = (shape, turn) => {
 let user1 = player("x", 1);
 let user2 = player("o", 0);
 const gameBoard = (() => {
+    let mode = "playerVSplayer";
+    const setMode = function(x){
+        mode=x;
+    }
+    const getMode = function(){
+        return mode;
+    }
     let bSquares = {
         s0: { id: 0, content: 0 },
         s1: { id: 1, content: 0 },
@@ -36,22 +43,22 @@ const gameBoard = (() => {
         mLine2: [bSquares.s2, bSquares.s4, bSquares.s6]
     };
     Array.prototype.random = function () {
-        return this[Math.floor((Math.random()*this.length))];
+        return this[Math.floor((Math.random() * this.length))];
     }
     const randomIndex = function () {
-        return Object.values(bSquares).filter(x=>x.content==0).map(x=>x.id).random();
+        return Object.values(bSquares).filter(x => x.content == 0).map(x => x.id).random();
     }
-    const checkWinningCondition= function(){
+    const checkWinningCondition = function () {
         for (let line in boardLines) {
             if (boardLines[line].reduce((a, b) => a + b.content, 0) == 2) {
                 return true;
             }
         }
     }
-    const fillRandomSquare = function(){
+    const fillRandomSquare = function () {
         let cpuSquareIndex = randomIndex();
         while (true) {
-            if (bSquares['s' + cpuSquareIndex].content !=0  )
+            if (bSquares['s' + cpuSquareIndex].content != 0)
                 cpuSquareIndex = randomIndex();
             else
                 break;
@@ -59,64 +66,63 @@ const gameBoard = (() => {
         const nextSquare = document.getElementById(Object.keys(bSquares).find(key => bSquares[key].id == cpuSquareIndex));
         nextSquare.textContent = user2.shape;
         fillSquaresObject(nextSquare, user2.shape);
-        nextSquare.removeEventListener("click", clickSquare.playerVScpu);
+        nextSquare.removeEventListener("click", gameMode[mode]);
     }
-    const clickSquare = {
+    const gameMode = {
         playerVSplayer: function () {
             if (user1.getTurn() == 1) {
                 this.textContent = user1.shape;
                 user1.toggleTurn();
                 user2.toggleTurn();
                 fillSquaresObject(this, user1.shape);
-                this.removeEventListener("click", clickSquare.playerVSplayer);
+                this.removeEventListener("click", gameMode.playerVSplayer);
                 checkWinner();
             } else if (user1.getTurn() == 0) {
                 this.textContent = user2.shape;
                 user2.toggleTurn();
                 user1.toggleTurn();
                 fillSquaresObject(this, user2.shape);
-                this.removeEventListener("click", clickSquare.playerVSplayer);
+                this.removeEventListener("click", gameMode.playerVSplayer);
                 checkWinner();
             }
         },
         playerVScpu: function () {
             this.textContent = user1.shape;
             fillSquaresObject(this, user1.shape);
-            this.removeEventListener("click", clickSquare.playerVScpu);
+            this.removeEventListener("click", gameMode.playerVScpu);
             const winnerStatus = checkWinner();
             //player played for the first time. cpu plays at random place.
             if (Object.values(bSquares).reduce((a, b) => a + b.content, 0) == 1) {
                 fillRandomSquare();
             }
-            else if (Object.values(bSquares).reduce((a, b) => a + b.content, 0) <1 && !winnerStatus) {
+            else if (Object.values(bSquares).reduce((a, b) => a + b.content, 0) < 1 && !winnerStatus) {
                 for (let line in boardLines) {
                     //if player is winning next turn
                     if (boardLines[line].reduce((a, b) => a + b.content, 0) == 2) {
-                        for(let item of boardLines[line]){
-                            if(item.content==0){
+                        for (let item of boardLines[line]) {
+                            if (item.content == 0) {
                                 let cpuSquareIndex;
-                                cpuSquareIndex=item.id;
+                                cpuSquareIndex = item.id;
                                 const nextSquare = document.getElementById(Object.keys(bSquares).find(key => bSquares[key].id == cpuSquareIndex));
                                 nextSquare.textContent = user2.shape;
                                 fillSquaresObject(nextSquare, user2.shape);
-                                nextSquare.removeEventListener("click", clickSquare.playerVScpu);
+                                nextSquare.removeEventListener("click", gameMode.playerVScpu);
                                 checkWinner();
                             }
                         }
                         break;
                     }
                     //if player isn't winning next turn
-                    else if(!checkWinningCondition()){
+                    else if (!checkWinningCondition()) {
                         fillRandomSquare();
                         checkWinner();
                         break;
-                    }                    
+                    }
                 }
             }
         }
     }
     const fillSquaresObject = function (that, playerShape) {
-        //fill squares object
         const currentSquareId = that.id;
         bSquares[currentSquareId].content = shape[playerShape];
     }
@@ -132,20 +138,54 @@ const gameBoard = (() => {
                 finishGame();
                 return true;
             }
-            else if(Object.values(bSquares).filter(x=>x.content==0).length==0){
+            else if (Object.values(bSquares).filter(x => x.content == 0).length == 0) {
                 console.log("tie");
                 finishGame();
                 return true;
             }
         }
     }
-    const boardSquareSelected = document.querySelectorAll(".board > div");
-    const finishGame = function () {
+    const start=function(modeValue){
+        modeValue=modeValue||mode;
+        setMode(modeValue);
+        const boardSquareSelected = document.querySelectorAll(".board > div");
         boardSquareSelected.forEach((square) => {
-            square.removeEventListener("click", clickSquare.playerVScpu);
+            square.addEventListener("click", gameMode[mode]);
         });
     }
-    boardSquareSelected.forEach((square) => {
-        square.addEventListener("click", clickSquare.playerVScpu);
-    });
+    const finishGame = function () {
+        const boardSquareSelected = document.querySelectorAll(".board > div");
+        boardSquareSelected.forEach((square) => {
+            square.removeEventListener("click", gameMode[mode]);
+        });
+    }
+    return {bSquares,start};
 })();
+const userInterface = (() =>{
+    //restart button
+    const restartButton = document.querySelector(".restart");
+    const restart=function(mode){
+        Object.values(gameBoard.bSquares).forEach(item =>{
+            item.content=0;
+        });
+        const boardSquares = document.querySelectorAll(".board > div");
+        boardSquares.forEach((square) => {
+            square.textContent="";
+        });
+        gameBoard.start(mode);
+        console.log(gameBoard.bSquares);
+    }
+    restartButton.addEventListener("click", ()=>{restart()});
+    //restart game with set mode
+    const vsPlayerButton=document.querySelector(".vsPlayer");
+    const vsPlayer=function(){
+        restart("playerVSplayer");
+    }
+    vsPlayerButton.addEventListener("click", vsPlayer);
+    const vsCpuButton=document.querySelector(".vsCpu");
+    const vsCpu=function(){
+        restart("playerVScpu");
+    }
+    vsCpuButton.addEventListener("click", vsCpu);
+})();
+gameBoard.start();
